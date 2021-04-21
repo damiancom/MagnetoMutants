b8be2f492c3f9cd8f23fdad53e642a7fbe467c3e
Damián Comba <damiancom@gmail.com>
2021-04-21 05:12:29 -0300

    New test classes are incorporated

---
+++ src/test/java/ar/com/meli/challenge/mutants/SearchMutantsApplicationTest.java
@@ -0,0 +1,11 @@
+package ar.com.meli.challenge.mutants;
+
+import org.junit.jupiter.api.Test;
+
+class SearchMutantsApplicationTest {
+
+    @Test
+    void main_launch_ok() {
+        SearchMutantsApplication.main(new String[] {});
+    }
+}
